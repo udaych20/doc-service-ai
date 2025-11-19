203e5d124b74a048ec4a8aba886e5cb2ed605dd6
Uday Chintala <udaych20@gmail.com>
2025-11-19 19:33:22 +0530

    google doc ai service

---
+++ src/test/java/com/test/doc/service/doc_service_ai/DocServiceAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.doc.service.doc_service_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocServiceAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
